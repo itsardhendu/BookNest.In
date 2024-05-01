4c89a8faafd2b2103b58f613dcccf14bef9dca2f
Ardhendu Sarkar <ardhendusarkar05@gmail.com>
2024-05-01 11:38:47 +0530

    FloatingActionButton Fixed

--- app/src/main/java/com/asr/bookstore/MainLayout.kt
+++ app/src/main/java/com/asr/bookstore/MainLayout.kt
@@ -1,6 +1,11 @@
 package com.asr.bookstore
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -8,6 +13,14 @@ import androidx.navigation.NavHostController
 @Composable
 fun MainLayout(navController: NavHostController, content: @Composable (PaddingValues) -> Unit) {
     Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /** navController.navTo(...) **/ },
+            ) {
+                Icon(Icons.Filled.ShoppingCart, "Cart")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
         bottomBar = { MyBottomAppBar(navController) },
         content = content
     )
